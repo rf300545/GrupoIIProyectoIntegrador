292919701a9167b71ce0ef351923e1f185234084
ElioPantoja <elio.pantoja@outlook.com>
2021-05-26 21:32:45 -0300

    primer app, css y html

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ("express")
+const app = express()
+const path =require ("path")
+
+
+app.use(express.static((__dirname + '/public'))); //Para ver HTML desde aca
+app.listen(3030,() => 
+console.log("ok"))
+
+app.get("/",(req,res)=> {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+});
+
