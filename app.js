86d9794275ab91597abe6c435ae1bf3c0bb4d5e8
Ruben Fernando Dieu <ARTB0805@carbocloro.com.br>
2021-05-30 19:54:21 -0300

    Agragados carrito

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.listen(3030,() =>
 console.log("ok"))
 
 app.get("/",(req,res)=> {
-    res.sendFile(path.join(__dirname, "/views/formregistro.html"))
+    res.sendFile(path.join(__dirname, "/views/carro_compras.html"))
 });
 
