74ff6df4af9b17fd72052df15ebd4b46907eac38
milagrosporta89--global <milagrosporta89@gmail.com>
2021-06-21 00:22:42 -0300

    se completo la estetica del formulario de create product

--- app.js
+++ app.js
@@ -26,3 +26,6 @@ app.get("/login",(req,res)=> {
 app.get("/producto",(req,res)=> {
     res.sendFile(path.join(__dirname, "/views/producto.html"))
 });
+app.get("/crearproducto",(req,res)=> {
+    res.sendFile(path.join(__dirname, "/views/createProduct.html"))
+});
