8e5a377b1566622701f815e4bacac2494f5bc024
Ruben Fernando Dieu <ARTB0805@carbocloro.com.br>
2021-06-21 20:09:18 -0300

    mvc final

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/productsController")
 
-router.get("/."); 
+// router.get("/."); 
 
 router.get("/carro_compras", productsController.ncarro);
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require ("../controllers/usersController")
 
-router.get("/."); 
+// router.get("/."); 
 
 router.get("/formregistro",userController.registrarse);
 
