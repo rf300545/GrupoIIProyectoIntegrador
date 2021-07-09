966a3812a988b0cb6bd0fd152822e29e0218b842
Ruben Fernando Dieu <ARTB0805@carbocloro.com.br>
2021-07-09 19:08:26 -0300

    anda products

--- src/routes/products.js
+++ src/routes/products.js
@@ -14,16 +14,7 @@ router.get("/producto", productsController.producto);
 router.get("/createProduct", productsController.createProducts);
 //router.post("/createProducts", productsController.ncreateProducts);
 
-<<<<<<< HEAD
 //VER DETALLE DE UN PRODUCTO
-=======
-/*Edicion de producto*/
-router.get("/editProduct", productsController.editProduct);
-//router.put('/edit/:id', productsController.update); /*????*/
-
-
-/*** VER DETALLE DE UN PRODUCTO ***/ 
->>>>>>> 3e3469a13c6316aeeb018350ff05e3a03f3a4ff4
 router.get("/producto/:id", productsController.idUnProducto); 
 
 
