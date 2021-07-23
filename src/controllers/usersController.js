6876cb525d8c8ee425b490c3f7e2c794109ed594
ElioPantoja <elio.pantoja@outlook.com>
2021-07-23 16:37:35 -0300

    modificado el user controller- falta

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const userFilePath = path.join(__dirname, '../database/userDB.json');
+const user = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
+
+
+
 const usersController = {
     iniciarSesion: (req, res) => {
         res.render("login");
@@ -5,6 +13,13 @@ const usersController = {
 
     registrarse: (req, res) => {
         res.render("register");
+    },
+    saveUser: (req, res) => {
+        let idNuevo = user[user.length-1].id + 1;
+        let newUser = Object.assign({id: idNuevo},req.body);;
+            user.push(newUser);
+            fs.writeFileSync(userFilePath, JSON.stringify(user,null, ' '));
+            res.redirect("/");
     }
 }
     module.exports = usersController
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require ("../controllers/usersController")
 
 router.get("/register",userController.registrarse);
+router.post("/register",userController.saveUser);
 
 router.get("/login",userController.iniciarSesion);
 
