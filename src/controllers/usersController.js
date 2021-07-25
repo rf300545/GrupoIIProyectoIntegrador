ab8b22ca7f7dbf5dcd73650b6a40aee5375c2c12
Ruben Fernando Dieu <ARTB0805@carbocloro.com.br>
2021-07-25 18:46:45 -0300

    rutas validator

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const userFilePath = path.join(__dirname, '../database/userDB.json');
 const user = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
+const { validacionCracionUsuarios } = require ( "express-validator");
 
 
 
@@ -13,13 +14,17 @@ const usersController = {
     registrarse: (req, res) => {
         res.render("register");
     },
-    
+
     saveUser: (req, res) => {
-        let idNuevo = user[user.length-1].id + 1;
-        let newUser = Object.assign({id: idNuevo},req.body);;
-            user.push(newUser);
-            fs.writeFileSync(userFilePath, JSON.stringify(user,null, ' '));
-            res.redirect("/");
+        //validacion creacion usuario
+        let errors = validacionCracionUsuarios (req);
+        res.send (errors);
+        //
+        // let idNuevo = user[user.length-1].id + 1;
+        // let newUser = Object.assign({id: idNuevo},req.body);;
+        //     user.push(newUser);
+        //     fs.writeFileSync(userFilePath, JSON.stringify(user,null, ' '));
+        //     res.redirect("/");
     }
 }
     module.exports = usersController
