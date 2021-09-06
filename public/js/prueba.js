6b2335694b38dcb511028e74fcd899a879d6c5da
unknown <milagrosporta89@gmail.com>
2021-09-05 22:11:32 -0300

    scritp para validacion de registro de usuario

--- public/js/prueba.js
+++ public/js/prueba.js
@@ -1,1 +1,52 @@
-console.log ("esto esta funcinoando")
+
+const form= document.getElementById("form")// no lo hice con querySelector porque ya esta el formulario de busqueda en la barra 
+const emailField = form.email
+const userPassword= form.contraseña // ver de sacarle la ñ en el html y en el contrlolador 
+const isEmail = (field)=>{
+    const regex= /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+    return regex.test (field.value.toLowerCase())// validacion para ver si es correcto el formato de email
+      }
+
+
+
+
+const isEmpty = (field) => {
+    return field.value.trim().length === 0
+    
+}
+
+const generateError = (field, message) => {
+    const span = field.nextElementSibling
+    span.innerHTML = message
+    span.classList.add("danger")// 
+    
+    
+}
+
+form.addEventListener ( "submit", function (e) {
+    let contador=[]
+     
+        if (isEmpty (emailField) == true  ){ //"" el string vacio se entinede como False por defecto
+          generateError (emailField,"El campo Email no puede estar vacio")
+          contador.push("email vacio")
+          console.log(contador)}
+          
+        if ( isEmpty(userPassword) != false ){
+            generateError (userPassword, "El campo Contraseña no puede estar vacio")
+            contador.push("contraseña vacia")
+            console.log(contador)}
+            
+
+        if (!isEmail(emailField)&& (!isEmpty(emailField))){
+            generateError (emailField,"Formato de email invalido")
+            contador.push("email invalido")
+            console.log(contador)}
+    
+        if (contador.length>0){
+
+           e.preventDefault ();
+        }}
+
+    
+    )
+    console.log ("esto esta fyuncionacndo")/* A VER COMO SALE ACA  */
---
+++ public/js/validationRegister.js
@@ -0,0 +1,75 @@
+const form= document.getElementById("form")// no lo hice con querySelector porque ya esta el formulario de busqueda en la barra 
+const firstNameField = form.first_name
+const lastNameField = form.last_name
+const emailField = form.email
+const passwordField = form.contraseña
+const confirmPassField= form.confirm_contraseña
+
+const isEmail = (field)=>{
+    const regex= /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+    return regex.test (field.value.toLowerCase())// validacion para ver si es correcto el formato de email
+      }
+const isEmpty = (field) => {
+    return field.value.trim().length === 0
+    }
+
+const generateError = (field, message) => {
+    const span = field.nextElementSibling
+    span.innerHTML = message
+    span.classList.add("danger")// 
+    
+    
+}
+const confirmacion= function(){
+    if (passwordField==confirmPassField){
+        
+    }
+}
+/* form.addEventListener ("submit", function (e){
+    e.preventDefault();
+    console.log ("esto creo que funciona")
+}) */
+
+form.addEventListener ( "submit", function (e) {
+    let contador=[]
+    e.preventDefault ();
+        if (isEmpty(firstNameField)==true){
+            generateError(firstNameField, "El campo nombre no puede estar vacío")
+            contador.push("nombre vacio ")
+         
+        }
+        if (isEmpty(lastNameField)==true){
+            generateError(lastNameField, "El campo apellido no puede estar vacío")
+            contador.push("apellido vacio")
+        }
+
+        if (isEmpty (emailField) == true  ){ //"" el string vacio se entinede como False por defecto
+          generateError (emailField,"El campo Email no puede estar vacio")
+          contador.push("email vacio")
+          }
+          
+        if (isEmpty(passwordField) ==true){
+            generateError (passwordField, "El campo Contraseña no puede estar vacio")
+            contador.push("contraseña vacia")}
+
+         if (isEmpty(confirmPassField) ==true){
+            generateError (confirmPassField, "El campo Contraseña no puede estar vacio")
+            contador.push("contraseña vacia")}
+         
+         if ( !isEmpty(confirmPassField) &&  (confirmPassField.value!==passwordField.value)){
+            generateError (confirmPassField, "la contraseña ingresada no coincide")
+             contador.push(confirmPassField.value)}    
+            
+
+        if (!isEmail(emailField)&& (!isEmpty(emailField))){
+            generateError (emailField,"Formato de email invalido")
+            contador.push("email invalido")
+    
+        if (contador.length>0){
+          
+           console.log (contador)
+        }}
+
+        
+    })
+    console.log ("esto esta fyuncionacndo")
