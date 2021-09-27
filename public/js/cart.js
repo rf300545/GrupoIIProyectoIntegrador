59a72fcaa924f25d91dfe1bb67b2b977694ac770
unknown <milagrosporta89@gmail.com>
2021-09-26 23:53:18 -0300

    carrito funcionando totalmente

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,12 +1,13 @@
 
-let pesca = document.getElementsByClassName ("row border-top border-bottom")/*  captura a donde se van a insertar los valores de localStorage */
+let selectedProduct = document.getElementsByClassName ("row border-top border-bottom")/*  captura a donde se van a insertar los valores de localStorage */
 let cartItems=localStorage.getItem("carrito")
+/* INSERCION DE LOCAL STORAGE EN HTML */
 function displayCart(){
 
     cartItems=JSON.parse(cartItems)
     
     Object.values(cartItems).map(item => {
-        pesca[0].innerHTML += ` <div class="row main align-items-center">
+        selectedProduct[0].innerHTML += ` <div class="row main align-items-center">
         <div class="col-2"><img class="img-fluid" src=${item.img}></div><!-- imagen del producto-- reemplazar -->
         <div class="col">
             <div class="row text-muted">${item.nombre}</div><!-- producto -->
@@ -14,7 +15,7 @@ function displayCart(){
         </div>
         <div class="col">
          <a href="#" class="resta"><i class="fas fa-minus fa-xs mp-cart-symbol minus"></i></a>
-         <a href="#" class="border-none" id="cantidad">1</a>
+         <a href="#" class="border-none" id="cantidad">${item.cantidad}</a>
          <a href="#" class="suma"><i class="fas fa-plus fa-xs mp-cart-symbol " ></i></a> </div>
         <div class="col">$ ${item.precio} <a href="#" class="cerrar">  <i class="far fa-trash-alt"></i></a></div>
         </div>`
@@ -27,53 +28,126 @@ displayCart()
 
 let btnPlus=document.querySelectorAll(".suma")
 let btnMinus=document.querySelectorAll(".resta")
-let inicio= 1
 
-/* boton menos */
 
-    Array.from(btnMinus).forEach((btn,i) => {
-    btn.addEventListener("click", () => {
-        
-         inicio= inicio-1
-         if(inicio<1){
-             inicio=1
-         }
-        let next=btnMinus[i].nextElementSibling.innerHTML = inicio.toString()
-    
-    })
-  
-    })
-/* boton mas */
-    Array.from(btnPlus).forEach((btn,i) => {
+    /* FUNCION RESTAR*/
+        Array.from(btnMinus).forEach((btn,i) => {
+            let inicio=1
         btn.addEventListener("click", () => {
+        let inicio=btnMinus[i].nextElementSibling.innerText
+            inicio= inicio-1
+            if(inicio<1){
+                inicio=1
+            }
+            cartItems[i].cantidad=inicio
+            localStorage.setItem("carrito",JSON.stringify (cartItems))
 
-        inicio= inicio+1
-        let next=btnMinus[i].nextElementSibling.innerHTML = inicio.toString()
-                 
-         })
-      
+            let next=btnMinus[i].nextElementSibling.innerHTML = inicio.toString() 
+            
+        })
+    
+        })
+    /* FUNCION AGREGAR */
+    
+      Array.from(btnPlus).forEach((btn,i) => {
+         btn.addEventListener("click", () => {
+            let inicio=parseInt(btnMinus[i].nextElementSibling.innerText)
+            inicio= inicio+1
+            cartItems[i].cantidad=inicio        
+            localStorage.setItem("carrito",JSON.stringify (cartItems))
+            let next=btnMinus[i].nextElementSibling.innerHTML = inicio.toString() 
+          
+         })})
+
+
+    /* FUNCION ELIMINAR */
+        let btnBorrar= document.getElementsByClassName("cerrar")
+        Array.from(btnBorrar).forEach((btn,i) => {
+            btn.addEventListener("click", () => {
+                let subtotal=localStorage.getItem("total_compra")
+                let montoARestar= cartItems[i].precio*cartItems[i].cantidad
+                let nuevoValorTotal= subtotal-montoARestar
+                localStorage.setItem("total_compra",nuevoValorTotal)
+                console.log(subtotal)
+
+                let btnId = i
+                newCart=cartItems.splice(0,btnId)
+                localStorage.setItem("carrito",JSON.stringify (newCart))
+                console.log(newCart)
+
+                window.location.reload(true);
+                
+            })  
+            
         });
+    
+    /* RESUMEN CARRITO */
+        let totalItems= document.getElementById ("qty-cart").innerHTML = "ITEMS "+ cartItems.length.toString() 
+        let cantProducts=document.getElementsByClassName ("border-none")
+        Array.from(cantProducts).forEach((cant,i) => {
+            console.log (cant)
+        
+            })  
+    /* OPERACIONES MATEMATICAS */
 
-/* delete del carrito */
-/* seteo para capturar boton eliminar  */
+    
 
-let btnBorrar= document.getElementsByClassName("cerrar")
-/* let cart= localStorage.getItem("carrito") */
-console.log(btnBorrar)
-console.log(cartItems)
+/*   function suma (){ */
+function suma(){
+    let total=0
 
- Array.from(btnBorrar).forEach((btn,i) => {
-    btn.addEventListener("click", () => {
-        let btnId = i
-        /* console.log( btnId) */
-        console.log(cartItems)
-        newCart=cartItems.splice(0,btnId)
-        console.log (newCart) *//* 
-        localStorage.setItem("carrito",JSON.stringify (newCart))
-        window.location.reload(true);
- 
-
+    Array.from(cartItems).forEach((producto)=>{
+        
+        let suma= producto.cantidad*producto.precio
+        total=total+suma
+       
+   })
   
-    })
-      
-});
+   localStorage.setItem("total_compra", total)
+    let totalCount= document.getElementById("total").innerHTML= "$ " + total.toString()
+   
+}
+suma() 
+
+
+    Array.from(btnPlus).forEach((btn,i) => {
+        btn.addEventListener("click",()=>{
+        let total=0
+        Array.from(cartItems).forEach((producto)=>{
+        
+             let suma= producto.cantidad*producto.precio
+             total=total+suma
+            
+        })
+        console.log(total)
+        let totalCount= document.getElementById("total").innerHTML= "$ " + total.toString()
+        localStorage.setItem("total_compra", total)
+    
+
+    })})
+    Array.from(btnMinus).forEach((btn,i) => {
+        btn.addEventListener("click",()=>{
+        let total=0
+        Array.from(cartItems).forEach((producto)=>{
+        
+             let suma= producto.cantidad*producto.precio
+             total=total+suma
+            
+        })
+        console.log(total)
+        let totalCount= document.getElementById("total").innerHTML= "$ " + total.toString()
+        localStorage.setItem("total_compra", total)
+    
+
+    })})
+    let totalCalc=localStorage.getItem("total_compra")
+    let totalCount= document.getElementById("total").innerHTML= "$ " + totalCalc.toString()
+  
+    
+
+
+
+/* let totalPrice=JSON.parse (localStorage.getItem("total_compra"))
+let totalCount= document.getElementById("total").innerHTML= "$ " + totalPrice.toString() */
+/*   function suma (){ */
+   
