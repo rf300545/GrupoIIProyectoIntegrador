759ebd4e1c9c54e4050595ca7e5ddb5b010fb044
unknown <milagrosporta89@gmail.com>
2021-09-28 11:21:00 -0300

    toggle para vista de productos

--- public/js/producto.js
+++ public/js/producto.js
@@ -1,2 +1,17 @@
-let modoDeUso=document.getElementById ("toggle")
-modoDeUso.addEventListener("click", console.log ("me apretaron"))
+let modoDeUso=document.querySelectorAll ("#toggle")
+let toggleText=document.querySelectorAll ("#toggleText")
+
+console.log 
+
+
+
+Array.from(modoDeUso).forEach((btn, i)=>{
+   btn.addEventListener("click", ()=>{
+       let texto= modoDeUso[i].nextElementSibling
+       if (texto.style.display === "none") {
+        texto.style.display = "block";
+      } else {
+        texto.style.display = "none";
+      }
+   })
+})
